0a68c22fcf7c99644eecbd8e971d7b9572454bbc
AAxxel <axelleonardo086@gmail.com>
2024-11-24 18:37:54 -0600

    Estamos desactivando las promociones que ya han expirado

---
+++ Tasks/desactivarPromociones.js
@@ -0,0 +1,44 @@
+const cron = require("node-cron");
+const { supabase } = require('../ruta/supabaseClient.js');
+
+const getPromosActivas = async () => {
+    const fechaActual = new Date();
+    try {
+        const { data: promos, error } = await supabase.from('Producto_promocion')
+        .update({estado: false})
+        .lte('fecha_final', fechaActual.toISOString())
+        .eq('estado', true)
+        .select('id');
+
+        if(error){
+            throw error;
+        }
+
+        if(promos.length === 0){
+            return { resultado: false,
+                promocionesDsc: []
+             }
+        }
+
+        return {resultado: true,
+            promocionesDsc: promos
+        };
+
+    } catch (error) {
+        console.error('Ocurrio un error: ', error);
+        return { resultado: false,
+            promocionesDsc: []
+         };
+    }
+}
+
+cron.schedule('10 1 * * *', async() => {
+    const { resultado, promocionesDsc } = await getPromosActivas();
+    if(resultado) {
+        console.log(`Las siguientes promociones fueron desactivadas por que su fecha expiro:`);
+        console.log(promocionesDsc);
+
+    }
+});
+
+module.exports = cron;
--- Tasks/startTasks.js
+++ Tasks/startTasks.js
@@ -1,10 +1,12 @@
 //const cronHolaMundo = require('./holaMundo.js');
 const rBsinGuardar = require('./restaurarRollBack.js');
 const rBsGuardado = require('./restaurarRollBackGuardado.js');
+const dscPromos = require('./desactivarPromociones.js');
 
 function startCrons(){
     rBsinGuardar;
     rBsGuardado;
+    dscPromos;
 }
 
 module.exports = startCrons;

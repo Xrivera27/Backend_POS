070bfbee48f0ef00a021162e931261920dfed626
AAxxel <axelleonardo086@gmail.com>
2024-11-28 00:09:39 -0600

    Estamos agregando sucursal y empresa al mismo tiempo

--- controllers/empresaController.js
+++ controllers/empresaController.js
@@ -1,8 +1,10 @@
 // Controlador para registrar una empresa
+const nodemailer = require('nodemailer');
 const register = async (req, res) => {
   const {
     nombre,
-    created_at,
+    nombre_usuario,
+    apellido_usuario,
     id_categoria,
     correo_principal,
     telefono_principal,
@@ -22,13 +24,14 @@ const register = async (req, res) => {
       .from('Empresas')
       .insert([{
         nombre: nombre,
-        created_at: created_at,
         id_categoria: id_categoria,
         correo_principal: correo_principal,
         telefono_principal: telefono_principal,
         usa_SAR: usa_SAR
       }])
-      .select();
+      .select('id_empresa');
+
+      const correo_perdomo = 'perdomoyasociados.registro@gmail.com';
 
     if (empresaError) {
       return res.status(500).json({
@@ -36,12 +39,53 @@ const register = async (req, res) => {
       });
     }
 
-    // Respuesta exitosa
-    res.status(200).json({
-      message: 'Empresa registrada exitosamente',
-      empresa
+    const { data: sucursal, error: sucursalError } = await supabase.from('Sucursales')
+    .insert({
+      nombre_administrativo: `${nombre} Sucursal Principal`,
+      id_empresa: empresa[0].id_empresa,
+      correo: correo_principal,
+      direccion: `EDITAR`,
+      telefono: telefono_principal,
+      estado: true
+    }).select('id_sucursal');
+
+    if (sucursalError) {
+      console.log(sucursalError);
+      return res.status(500).json({
+        error: 'Error al registrar la sucursal principal: ' + sucursalError.message
+      });
+    }
+
+    const dato_usuarios = {
+      nombre_usuario: nombre_usuario,
+      apellido_usuario: apellido_usuario,
+      correo: correo_principal,
+      telefono_principal: telefono_principal,
+      id_sucursal: sucursal[0].id_sucursal
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
     });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: correo_perdomo,
+      subject: 'Solicitud de registro de nueva empresa',
+      text: `Estimado(a),\n\nPor favor, proceda a registrar al usuario en el sistema utilizando los datos proporcionados a continuación:\n\nNombre: ${dato_usuarios.nombre_usuario}\nApellido: ${dato_usuarios.apellido_usuario}\nCorreo Electrónico: ${dato_usuarios.correo}\nTeléfono: ${dato_usuarios.telefono_principal}\nID de Sucursal Principal: ${dato_usuarios.id_sucursal}\n\nAgradecemos que confirme el registro una vez completado. Si tiene alguna duda o requiere asistencia durante el proceso, no dude en contactarnos.\n\nAtentamente,\nEl equipo de soporte.\n`
+
+    };
+
+    // Enviar el correo
+    await transporter.sendMail(mailOptions);
+
+
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       error: 'Error inesperado al registrar la empresa'
     });

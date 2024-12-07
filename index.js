f0e5ef264ad7559708dedbd650b1a0b315020dbe
AAxxel <axelleonardo086@gmail.com>
2024-12-07 16:49:48 -0600

    cambiamos puerto

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api', routes); // Usar el index de rutas
 tareasArranque();
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
